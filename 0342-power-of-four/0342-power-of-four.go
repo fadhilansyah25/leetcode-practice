378a37845d72a884d5b8b2a8ca07b470584a48b3
Fadil Ardiansyah <fadhilansyah25@gmail.com>
2023-10-16 12:48:36 +0700

    Time: 0 ms (100%), Space: 2.1 MB (76.74%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.go
+++ 0342-power-of-four/0342-power-of-four.go
@@ -1,6 +1,15 @@
 func isPowerOfFour(n int) bool {
-    for n > 0 && n % 4 == 0 {
+    if n == 1 {
+        return true
+    }
+
+    if n < 4 {
+        return false
+    }
+
+    for n % 4 == 0 {
         n /= 4
     }
+
     return n == 1
 }
