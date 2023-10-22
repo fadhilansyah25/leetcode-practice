6b2e76869d4903e21b931157694e4938752a089f
Fadil Ardiansyah <fadhilansyah25@gmail.com>
2023-10-22 15:15:09 +0700

    Time: 3 ms (60.98%), Space: 2.5 MB (40.97%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.go
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.go
@@ -6,15 +6,31 @@
  * }
  */
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-    head := &ListNode{}
-    currHead := head
+    if list1 == nil {
+        return list2
+    }
 
+    if list2 == nil {
+        return list1
+    }
+
+
+    var head *ListNode
+    if list1.Val < list2.Val {
+        head = list1
+        list1 = list1.Next
+    } else {
+        head = list2
+        list2 = list2.Next
+    }
+
+    currHead := head
     for list1 != nil && list2 != nil {
         if list1.Val < list2.Val {
-            currHead.Next = &ListNode{Val: list1.Val}
+            currHead.Next = list1
             list1 = list1.Next
         } else {
-            currHead.Next = &ListNode{Val: list2.Val}
+            currHead.Next = list2
             list2 = list2.Next
         }
         currHead = currHead.Next
@@ -23,9 +39,11 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 
     if list1 == nil {
         currHead.Next = list2
-    } else {
-        currHead.Next = list1
-    }
+    } 
 
-    return head.Next
+    if list2 == nil {
+        currHead.Next = list1
+    } 
+
+    return head
 }   
