4a2e21c34b0529823b71afc9a502d7170528d5ac
Fadil Ardiansyah <fadhilansyah25@gmail.com>
2024-08-07 22:03:15 +0700

    Time: 0 ms (100.00%) | Memory: 2.4 MB (64.41%) - LeetSync

--- 27-remove-element/remove-element.go
+++ 27-remove-element/remove-element.go
@@ -1,10 +1,4 @@
 func removeElement(nums []int, val int) int {
-	// nums = append(nums[:1], nums[2:]...)
-	// for i, v := range nums {
-	// 	if v == val {
-	// 		nums = append(nums[:i], nums[i+1:]...)
-	// 	}
-
 	i := 0
 	for i < len(nums) {
 		if nums[i] == val {
@@ -13,8 +7,6 @@ func removeElement(nums []int, val int) int {
 			i++
 		}
 	}
-
-	fmt.Println(nums)
 
 	return len(nums)
 }
