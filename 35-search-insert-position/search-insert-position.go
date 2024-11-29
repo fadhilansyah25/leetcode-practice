aeb0ba719595db0c750654b780fdbf9338668461
Fadil Ardiansyah <fadhilansyah25@gmail.com>
2024-11-29 10:22:45 +0700

    Time: 0 ms (100.00%) | Memory: 4.9 MB (13.37%) - LeetSync

--- 35-search-insert-position/search-insert-position.go
+++ 35-search-insert-position/search-insert-position.go
@@ -2,9 +2,9 @@ func searchInsert(nums []int, target int) int {
     var pos int = 0;
 
     for pos < len(nums) {
-        if nums[pos] == target {
-            break;
-        }
+        // if nums[pos] == target {
+        //     break;
+        // }
 
         if nums[pos] < target {
             pos++;
