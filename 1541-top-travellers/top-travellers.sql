f38a44e8d44465a2525c9651f5f1aadb5104e897
Fadil Ardiansyah <fadhilansyah25@gmail.com>
2025-01-02 11:39:58 +0700

    Time: 704 ms (76.29%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1541-top-travellers/top-travellers.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+SELECT 
+    U.name,
+    COALESCE(SUM(R.distance), 0) AS travelled_distance
+FROM 
+    Users AS U 
+    LEFT JOIN Rides AS R ON U.id = R.user_id
+GROUP BY U.id, U.name
+ORDER BY travelled_distance DESC, U.name ASC;
