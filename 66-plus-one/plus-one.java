4c9360b9a1de7eb83c7fa471725f626a0413b692
Fadil Ardiansyah <fadhilansyah25@gmail.com>
2025-01-10 08:31:08 +0700

    Time: 0 ms (100.00%) | Memory: 41.8 MB (73.39%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,33 +1,51 @@
+// class Solution {
+//     public int[] plusOne(int[] digits) {
+//         if(digits[digits.length - 1] < 9) {
+//             digits[digits.length - 1]++;
+//             return digits;
+//         }
+
+//         ArrayList<Integer> res = new ArrayList<Integer>();
+
+//         int carry = 1;
+
+//         for(int i = digits.length - 1; i >= 0; i--) {
+//             int sum  = carry + digits[i];
+
+//             if(sum == 10) {
+//                 res.add(0,0);
+//             } else {
+//                 res.add(0, digits[i] + carry);
+//                 carry = 0;
+//             }
+//         }
+//         if(carry != 0) res.add(0, 1);
+
+//         // convert into new array from ArrayList
+//         int[] result = new int[res.size()];
+
+//         for(int i = 0; i < res.size(); i++) {
+//             result[i] = res.get(i);
+//         }
+
+//         return result;
+//     }
+// }
+
 class Solution {
     public int[] plusOne(int[] digits) {
-        if(digits[digits.length - 1] < 9) {
-            digits[digits.length - 1]++;
-            return digits;
-        }
+       int n = digits.length;
 
-        ArrayList<Integer> res = new ArrayList<Integer>();
-
-        int carry = 1;
-
-        for(int i = digits.length - 1; i >= 0; i--) {
-            int sum  = carry + digits[i];
-
-            if(sum == 10) {
-                res.add(0,0);
-            } else {
-                res.add(0, digits[i] + carry);
-                carry = 0;
+       for(int i = n - 1; i >=0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
             }
-        }
-        if(carry != 0) res.add(0, 1);
+            digits[i] = 0;
+       }
 
-        // convert into new array from ArrayList
-        int[] result = new int[res.size()];
-
-        for(int i = 0; i < res.size(); i++) {
-            result[i] = res.get(i);
-        }
-
-        return result;
+       int[] result = new int[n+1];
+       result[0] = 1;
+       return result;
     }
 }
